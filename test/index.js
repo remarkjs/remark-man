cc27530dd3375c2dda77a0213855fea0f9dee2f7
Titus Wormer <tituswormer@gmail.com>
2015-08-05 19:23:47 +0200

    Update mdast

--- test/index.js
+++ test/index.js
@@ -10,7 +10,7 @@ var path = require('path');
 var fs = require('fs');
 var assert = require('assert');
 var mdast = require('mdast');
-var File = require('mdast/lib/file');
+var File = require('vfile');
 var man = require('..');
 
 /*
